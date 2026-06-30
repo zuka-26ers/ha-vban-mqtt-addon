72afe429736e3b30f02fc33ae9f1fbdb72ae4796
zuka-26ers <65401914+zuka-26ers@users.noreply.github.com>
2026-06-30 23:47:05 +0900

    del old folder

--- ha-vban-mqtt-addon/vban_mqtt_notuse/index.js
+++
@@ -1,8 +0,0 @@
-console.log('VBAN MQTT Add-on starting...');
-
-// TODO: implement MQTT ⇄ VBAN bridge
-// - connect to MQTT broker
-// - subscribe to vban/* topics
-// - send UDP packets to Voicemeeter (VBAN text protocol)
-
-console.log('Config loaded (placeholder add-on)');
